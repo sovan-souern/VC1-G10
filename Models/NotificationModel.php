ed881b149e4b25bc5d5a1fba86e56421a346433d
yon ratana <yonratana71@gmail.com>
2025-03-11 15:14:21 +0700

    feat(notification):add notification[G1-154]

---
+++ Models/NotificationModel.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'Databases/database.php';
+class NotificationModel {
+
+    private $conn;
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function getNotifications() {
+        $sql = "SELECT * FROM notifications WHERE is_deleted = FALSE";
+        $stmt = $this->conn->query($sql);
+
+        $notifications = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $notifications[] = $row;
+        }
+
+        return $notifications;
+    }
+
+    public function addNotification($title, $message) {
+        $sql = "INSERT INTO notifications (title, message, is_deleted) VALUES (?, ?, FALSE)";
+        $stmt = $this->conn->prepare($sql);
+        if ($stmt->execute([$title, $message])) {
+            echo "<script>console.log('Notification added successfully');</script>";
+        } else {
+            echo "<script>console.log('Failed to add notification');</script>";
+        }
+    }
+
+    public function closeConnection() {
+        $this->conn = null;
+    }
+}
+?>
