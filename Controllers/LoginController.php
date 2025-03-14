9d5551b7db46b74acaaba078c1209e764d6ab623
sovan-souern <sovan.souern@student.passerellesnumeriques.org>
2025-03-14 19:15:59 +0700

    Your commit message

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -1,28 +1,16 @@
 <?php
 
-// class LoginController  extends BaseController {
-//     public function login() {
-//         require "views/authentication/login.php";
-
-//     }
-// }
-
-require_once "BaseController.php";
-
-class WelcomeController extends BaseController {
-    public function welcome() {
-        $this->views('views/welcome/welcome.php');
+class LoginRegisterController extends BaseController
+{
+    public function login () 
+    {
+        require "Views/auth/login.php";
     }
-    public function login() {
-        $this->views('views/auth/login.php');
-    }
-    public function register() {
-        $this->views('views/auth/register.php');
-    }
-    public function logout() {
-        session_start();
-        session_destroy();
-        header("Location: views/auth/login.php");
-        exit;
+
+    public function register() 
+    {
+        require "Views/auth/register.php";
     }
 }
+
+?>
