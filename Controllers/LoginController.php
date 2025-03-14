bed4cf4620880ebadf7604c62330d7cdaa01eba2
sovan-souern <sovan.souern@student.passerellesnumeriques.org>
2025-03-14 20:40:00 +0700

    Updated LoginController.php and routes.php

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -1,28 +1,17 @@
 <?php
-require_once "BaseController.php";
-class LoginController  extends BaseController {
-    public function login() {
-        require "login.php";
-
+require_once 'Models/UserModel.php';
+require_once 'BaseController.php';
+class LoginRegisterController extends BaseController
+{
+    public function login () 
+    {
+        require "Views/auth/login.php";
+    }
+    
+    public function register() 
+    {
+        require "Views/auth/register.php";
     }
 }
 
-// require_once "BaseController.php";
-
-// class WelcomeController extends BaseController {
-//     public function welcome() {
-//         $this->views('views/welcome/welcome.php');
-//     }
-//     public function login() {
-//         $this->views('views/auth/login.php');
-//     }
-//     public function register() {
-//         $this->views('views/auth/register.php');
-//     }
-//     public function logout() {
-//         session_start();
-//         session_destroy();
-//         header("Location: views/auth/login.php");
-//         exit;
-//     }
-// }
+?>
