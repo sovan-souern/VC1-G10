08baa656c4a38d939418fd5251a68a255e191243
panha <panha.nhean@student.passerellesnumeriques.org>
2025-03-18 14:06:06 +0700

    style(profile):addstyle to profile

--- Views/layout/nav.php
+++ Views/layout/nav.php
@@ -259,11 +259,12 @@
                   notifications
                 </span>
             </li> -->
-<!-- User -->
+<!-- User --><!-- User Profile Dropdown -->
 <li class="nav-item navbar-dropdown dropdown-user dropdown">
   <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
     <div class="avatar avatar-online">
-      <img src="<?php echo !empty($_SESSION['profile_picture']) ? '/' . $_SESSION['profile_picture'] : '/Views/assets/img/avatars/1.png'; ?>" alt class="w-px-40 h-auto rounded-circle" />
+      <img src="<?php echo !empty($_SESSION['profile_picture']) ? '/' . $_SESSION['profile_picture'] : '/Views/assets/img/avatars/1.png'; ?>" 
+           alt="User Profile" class="profile-img" />
     </div>
   </a>
   <ul class="dropdown-menu dropdown-menu-end">
@@ -272,11 +273,14 @@
         <div class="d-flex">
           <div class="flex-shrink-0 me-3">
             <div class="avatar avatar-online">
-              <img src="<?php echo !empty($_SESSION['profile_picture']) ? '/' . $_SESSION['profile_picture'] : '/Views/assets/img/avatars/1.png'; ?>" alt class="w-px-40 h-auto rounded-circle" />
+              <img src="<?php echo !empty($_SESSION['profile_picture']) ? '/' . $_SESSION['profile_picture'] : '/Views/assets/img/avatars/1.png'; ?>" 
+                   alt="User Profile" class="profile-img" />
             </div>
           </div>
           <div class="flex-grow-1">
-            <span class="fw-semibold d-block"><?php echo htmlspecialchars($_SESSION['name'] ?? 'John Doe'); ?></span>
+            <span class="fw-semibold d-block">
+              <?php echo htmlspecialchars($_SESSION['name'] ?? 'John Doe'); ?>
+            </span>
             <small class="text-muted">Admin</small>
           </div>
         </div>
@@ -310,7 +314,8 @@
     </li>
   </ul>
 </li>
-<!--/ User -->
+<!--/ User Profile Dropdown -->
+
           </ul>
         </div>
       </nav>
