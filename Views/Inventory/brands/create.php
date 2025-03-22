92ecac1028628be8632630781126fd30ca84d94b
Suon Samoun <samoun.suon@student.passerellesnumeriques.org>
2025-03-22 17:26:25 +0700

    style (change layout brand) change layout brand create and do responsive [G1-196]

--- Views/Inventory/brands/create.php
+++ Views/Inventory/brands/create.php
@@ -3,16 +3,16 @@
             <h4>Add Brand</h4>
             <form class="my-3" action="/brand/store" method="POST" enctype="multipart/form-data">
                 <div class="row">
-                    <div class="col-md-12 mb-3">
+                    <div class="input-name">
                         <label>Brand Name</label>
                     <input type="text" class="form-control" name="brand_name">
                 </div>
-                <div class="col-md-6">
-                    <label class="">Brand Content</label>
+                <div class="input-description" >
+                    <label class="">Brand Description</label>
                     <textarea class="form-control mt-2 p-4" style="height: 100px;" name="description"></textarea>
                 </div>
-                <div class="col-lg-6 md-3">
-                    <div class="form-group">
+                <div class="input-image">
+                    <div class="form-group" style="height: 70%;">
                         <label>Brand Image</label>
                         <div class="image-upload">
                             <input type="file" name="image">
@@ -23,11 +23,84 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="button-group">
                     <button type="submit" class="btn btn-success">Submit</button>
-                                            <button type="button" class="btn btn-warning">Back</button>
+                                            <button type="button"  onclick="window.history.back()"class="btn btn-warning">Back</button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
+    <style>
+    .row {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-areas:
+            "name name image"
+            "description description image"
+            "buttons buttons image";
+        gap: 10px;
+    }
+
+    .input-name {
+        grid-area: name;
+    }
+
+    .input-description {
+        grid-area: description;
+    }
+
+    .input-image {
+        grid-area: image;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100%;
+    }
+
+    .button-group {
+        grid-area: buttons;
+        display: flex;
+        justify-content: flex-start; 
+        text-align: left; 
+        gap: 10px;
+    }
+
+    label {
+        font-weight: bold;
+        margin-bottom: 5px;
+    }
+
+    .image-upload {
+        height: 100%;
+    }
+
+    .image-uploads {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 10px;
+        flex-grow: 1; 
+    }
+
+    .image-uploads img {
+        max-width: 100%;
+        height: auto;
+        width: 100px;
+    }
+
+    @media (max-width: 600px) {
+        .row {
+            grid-template-columns: 1fr;
+            grid-template-areas:
+                "name"
+                "description"
+                "image"
+                "buttons";
+        }
+        .image-uploads img {
+        width: 70px;
+    }
+    }
+</style>
