d4884fb0e23723997a41c14fe82ea585bd9fbe21
panha <panha.nhean@student.passerellesnumeriques.org>
2025-04-02 10:17:25 +0700

    refactor(ShoponwerMenagement):Change form create shoponwer in layout and display role on nav and style on reset Password

--- Views/E-commerce-user/home/home.php
+++ Views/E-commerce-user/home/home.php
@@ -291,14 +291,10 @@
         }
 
 
-<<<<<<< HEAD
         body {
             
             padding: 20px;
         }
-=======
-
->>>>>>> 9469813a5b1c3a0ff94b34ff8c96e2018c623373
 
         .products-container {
             display: grid;
