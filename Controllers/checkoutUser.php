5a73f934cdd6613302bdfaa3b6add5a73c407244
sovan-souern <sovan.souern@student.passerellesnumeriques.org>
2025-04-08 20:56:19 +0700

    feat(checkout) add new checkout ABA AC and Wing Bank[G1-220]

--- Controllers/checkoutUser.php
+++ Controllers/checkoutUser.php
@@ -24,11 +24,12 @@ class CheckoutUserController extends BaseController
 
     $admin_id = $users[0]['admin_id'] ?? null;
 
-    $this->ViewsUser('E-commerce-user/card/checkout.php', [
+    extract([
       "users" => $users,
       "products" => $products,
-      "admin_id" => $admin_id // Pass admin_id to the view
-    ]);
+      "admin_id" => $admin_id
+  ]);
+  require_once 'Views/E-commerce-user/card/checkout.php';
   }
 
 
