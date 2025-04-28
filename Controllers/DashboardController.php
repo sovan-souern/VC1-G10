f0a2afb2734f78c7d716253ae901f83f1dd5c8ef
sovan-souern <sovan.souern@student.passerellesnumeriques.org>
2025-04-28 20:08:17 +0700

    fix error

--- Controllers/DashboardController.php
+++ Controllers/DashboardController.php
@@ -1,9 +1,30 @@
 <?php
 require_once 'Controllers/BaseController.php';
 
-class DashboardController extends BaseController{
-        function index(){
-            $this->views('dashboard/list.php');
-            
-        }
+class DashboardController extends BaseController {
+    function index() {
+        // Fetch data (replace with actual database queries)
+        $data = [
+            'profit' => 5000,
+            'sales' => 12000,
+            'payments' => 8000,
+            'orders' => 300,
+            'totalRevenue' => [
+                '2024' => 40000,
+                '2025' => 50000
+            ],
+            'orderStatistics' => [
+                'totalOrders' => 8258,
+                'categories' => [
+                    ['name' => 'Electronic', 'sales' => '82.5k'],
+                    ['name' => 'Fashion', 'sales' => '23.8k'],
+                    ['name' => 'Decor', 'sales' => '849k'],
+                    ['name' => 'Sports', 'sales' => '99']
+                ]
+            ]
+        ];
+
+        // Pass data to the view
+        $this->views('dashboard/list.php', $data);
+    }
 }
